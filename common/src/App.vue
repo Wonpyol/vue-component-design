a6f339d0a011a605483028a68c41d70fcca05e6f
wonpyolee <wonpyol@gmail.com>
2023-06-30 13:38:00 +0900

    init

---
+++ common/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <app-header :title="appTitle"></app-header>
+    <app-contents :items="items" @renew="renewItems"></app-contents>
+  </div>
+</template>
+<script>
+import {defineComponent} from "vue";
+import AppHeader from "@/components/AppHeader.vue";
+import AppContents from "@/components/AppContents.vue";
+
+export default defineComponent({
+  components: {AppContents, AppHeader},
+  data() {
+    return {
+      appTitle: 'Common Approach',
+      items: [10, 20, 30],
+    }
+  },
+  methods: {
+    renewItems() {
+      this.items = [40, 50, 60];
+    },
+  },
+})
+</script>
+<style scoped>
+</style>
---
+++ slots/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템1</item>
+      <item>
+        <small>아이템2</small>
+      </item>
+      <item>
+        <small>아이템3</small>
+        <button>버튼</button>
+      </item>
+    </ul>
+
+  </div>
+</template>
+<script>
+import item from "@/components/Item.vue";
+export default {
+  components:{
+    item
+  },
+}
+</script>
+
+<style scoped>
+</style>
